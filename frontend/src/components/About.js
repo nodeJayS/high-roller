3ba7afe38389870c94629be1e713e9171a8225d7
J1-js <shinjayw@gmail.com>
2020-08-23 15:38:16 -0400

    Adjusted about component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -21,11 +21,16 @@ export default class About extends Component {
                 <div className='nav-div' onClick={this.toggleModal}><i className="fas fa-question-circle"></i> ABOUT</div>
                 <Modal show={this.state.show} onHide={this.toggleModal} size='lg'>
                     <Modal.Header>
-                        <Modal.Title>ABOUT US</Modal.Title>
+                        <Modal.Title>ABOUT HIGH-ROLLER</Modal.Title>
                     </Modal.Header>
 
                     <Modal.Body>
-                        <div>High-roller is a provably fair dice game with a 1% house edge.</div>
+                        <div>
+                            High-roller is a provably fair dice game with a 1% house edge.
+                            The outcome is generated through a combination of a client seed, server seed and server seed hash.
+                            The server seed hash is processed through a SHA512 algorithm.
+                            All outcomes are in a range from 1-100.
+                        </div>
                     </Modal.Body>
 
                     <Modal.Footer>
